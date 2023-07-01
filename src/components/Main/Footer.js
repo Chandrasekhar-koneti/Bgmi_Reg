1d83c4ae242219e930f3b40da18a16ecb86b0437
Chandrasekhar-koneti <konetichandra123@gmail.com>
2023-07-01 16:13:32 +0530

    footer updated

--- src/components/Main/Footer.js
+++ src/components/Main/Footer.js
@@ -1,15 +1,21 @@
 import { Fragment } from "react"
 import classes from './footer.module.css'
+import './footer.css'
 
 
 const Footer=()=>{
     return(
         <Fragment>
-            <div className={classes.footer}>
-                <h2>enjoy the new features</h2>
-                {/* <img src={yt} alt="yt"></img> */}
-  
-            </div>
+            <footer className="footer">
+      <div className="footer-content">
+        <p>&copy; 2023 76 SNAKE. All rights reserved.</p>
+        <div className="footer-links">
+          <a href="/">Home</a>
+          <a href="/about">About</a>
+          <a href="/contact">Contact</a>
+        </div>
+      </div>
+    </footer>
 
         </Fragment>
     )
